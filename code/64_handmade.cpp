935b2a52c97f20c9b79c08dbbca7a87d8ab9435e
Axel Daguerre <axeldaguerre@protonmail.com>
2022-11-02 09:12:14 +0100

    Game Controller && Keyboard

--- code/64_handmade.cpp
+++ code/64_handmade.cpp
@@ -1,15 +1,18 @@
 #include <windows.h>
 #include <stdint.h>
-
+#include <xinput.h>
+// NOTE For a refresher you can go to https://youtu.be/w7ay7QXmo_o?t=4582 
+// explainning well all basic code here
 typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
-
+// NOTE(axel): When using bool, compiler during optimization has to convert 
+// the binary to 1 or 0 value. With this type we only care about any bit value or 0.
+typedef uint32 bool32;
 #define internal static // function file scoped
 #define global_variable static
 #define local_persist static
-
 
 struct Win32_offscreen_buffer { // Bitmap
 	BITMAPINFO Info;
@@ -25,10 +28,47 @@ struct Win32_window_dimensions {
 	int Height;
 };
 
-global_variable bool Running; // static initialize to zero automatically
+//NOTE(axel): RARE TRICK to swap a function from a library to your own, id est if 
+// you can't be 100% be sure that the function will be available and cause an error
+// you create function in your file that will swap the real one to your own, tipically 
+// you will have an if statement to know if its available then if not provide yours, otherwise just 
+// use the real one. As C doesn't allowed several function on the same name and signature, you have 
+// to trick the compiler. (i.e XInputGetState_ / XInputGetState)
+#define X_INPUT_GET_STATE(name) DWORD name(DWORD dwUserIndex,  XINPUT_STATE* pState)
+typedef X_INPUT_GET_STATE(x_input_get_state);
+X_INPUT_GET_STATE(XInputGetStateStub)
+{
+	return(ERROR_DEVICE_NOT_CONNECTED);
+}
+global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
+#define  XInputGetState XInputGetState_
+
+
+// XInputSetState
+#define X_INPUT_SET_STATE(name) DWORD name(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration)
+typedef X_INPUT_SET_STATE(x_input_set_state);
+X_INPUT_SET_STATE(XInputSetStateStub)
+{
+	return(ERROR_DEVICE_NOT_CONNECTED);
+}
+global_variable x_input_set_state  *XInputSetState_ = XInputSetStateStub;
+#define XInputSetState XInputSetState_
+
+global_variable bool GlobalRunning; // static initialize to zero automatically
 global_variable Win32_offscreen_buffer GlobalBackBuffer;
 
-Win32_window_dimensions GetWindowDimensions(HWND Window) {
+internal void LoadXInput(void)
+{
+	HMODULE XInputLibrary = LoadLibraryA("xinput1_3.dll");
+	if (XInputLibrary)
+	{
+		XInputGetState = (x_input_get_state *) GetProcAddress(XInputLibrary, "XInputGetState");
+		XInputSetState = (x_input_set_state *) GetProcAddress(XInputLibrary, "XInputSetState");
+
+	}
+}
+
+Win32_window_dimensions Win32GetWindowDimensions(HWND Window) {
 	Win32_window_dimensions Result;
 	HDC DeviceContext = GetDC(Window);
 	RECT ClientRect;
@@ -39,9 +79,9 @@ Win32_window_dimensions GetWindowDimensions(HWND Window) {
 }
 
 internal void RenderWeirdGradient(Win32_offscreen_buffer Buffer, int BlueOffset, int RedOffset) {
-	int Width = Buffer.Width;
+	int Width  = Buffer.Width;
 	int Height = Buffer.Height;
-	int Pitch = Buffer.Width * Buffer.BytesPerPixel;
+	int Pitch  = Buffer.Width * Buffer.BytesPerPixel;
 	uint8 *Row = (uint8*)Buffer.Memory;
 	for (int Y = 0;
 		Y < Height;
@@ -62,7 +102,8 @@ internal void RenderWeirdGradient(Win32_offscreen_buffer Buffer, int BlueOffset,
 	}
 }
 
-internal void Win32ResizeDIBSection(Win32_offscreen_buffer *Buffer, int Width, int Height)
+internal void 
+Win32ResizeDIBSection(Win32_offscreen_buffer *Buffer, int Width, int Height)
 {
 	if (Buffer->Memory)
 	{
@@ -71,7 +112,9 @@ internal void Win32ResizeDIBSection(Win32_offscreen_buffer *Buffer, int Width, i
 	Buffer->Width = Width;
 	Buffer->Height = Height;
 	Buffer->BytesPerPixel = 4;
-
+	// NOTE(axel) When biHeight is negative it's a clue to Windows that 
+	// its a top-down DIB (see doc). Meaning that the first three bytes 
+	// are the color for the top left pixel, not the bottom-left.
 	Buffer->Info.bmiHeader.biSize = sizeof(Buffer->Info.bmiHeader);
 	Buffer->Info.bmiHeader.biHeight = -Buffer->Height;
 	Buffer->Info.bmiHeader.biWidth = Buffer->Width;
@@ -88,8 +131,8 @@ internal void Win32DisplayBufferInWindow(HDC DeviceContext,
 										 Win32_offscreen_buffer Buffer)
 {
 	StretchDIBits(DeviceContext,
-				  0, 0, Buffer.Width, Buffer.Height,
 				  0, 0, WindowWidth, WindowHeight,
+				  0, 0, Buffer.Width, Buffer.Height,
 				  Buffer.Memory,
 				  &Buffer.Info,
 				  DIB_RGB_COLORS, SRCCOPY);
@@ -104,21 +147,70 @@ LRESULT Win32MainWindowCallBack(
 	LRESULT result = 0;
 	switch (Message)
 	{
+		// NOTE(axel): Normally SYSKEY case is handle by DEFAULT but if you handle it here
+		// you loose the default handle.
+		case WM_SYSKEYDOWN:
+		case WM_SYSKEYUP:
+		case WM_KEYDOWN:
+		case WM_KEYUP:
+		{
+			uint32 VKCode = WParam;
+			// NOTE(axel): easy mistake -> Watch out on precedence operations '!=' evaluate
+			// before '&' so without proper parenthesis your evaluation will be wrong.
+			bool WasDown = ((LParam & (1 << 30)) != 0);
+			bool IsDown = ((LParam & (1 << 31)) == 0);
+
+			if (WasDown != IsDown)
+				{
+				if (VKCode == 'A') 
+				{
+					OutputDebugStringA("A\n");
+				}
+				else if (VKCode == 'W')
+				{
+					OutputDebugStringA("W\n");
+				}
+				else if (VKCode == 'D')
+				{
+					OutputDebugString("D\n");
+				}
+				else if (VKCode == 'S')
+				{
+					OutputDebugStringA("S\n");
+				}
+				else if (VKCode == VK_SPACE)
+				{
+					OutputDebugStringA("SPACE\n");
+				}
+				else if (VKCode == VK_ESCAPE)
+				{
+					GlobalRunning = false;
+					OutputDebugStringA("ESC\n");
+				}
+
+				bool32 AltKeyIsDown = LParam & (1 << 29);
+
+				if (VKCode == VK_F4 && AltKeyIsDown)
+				{
+					OutputDebugString("ALT");
+					GlobalRunning = false;
+				}
+			}
+			break;
+		}
 		case WM_DESTROY:
 		{
-			Running = false;
+			GlobalRunning = false;
 			OutputDebugStringA("DESTROY\n");
 		} break;
 		case WM_SIZE:
 		{
-			Win32_window_dimensions WindowDimensions = GetWindowDimensions(Window);
-			Win32ResizeDIBSection(&GlobalBackBuffer, WindowDimensions.Width, WindowDimensions.Width);
 			OutputDebugStringA("SIZE\n");
 		} break;
 		case WM_CLOSE:
 		{
 			OutputDebugStringA("CLOSE\n");
-			Running = false;
+			GlobalRunning = false;
 		} break;
 		case WM_ACTIVATEAPP:
 		{
@@ -126,23 +218,23 @@ LRESULT Win32MainWindowCallBack(
 		} break;
 		case WM_PAINT:
 		{
-			OutputDebugStringA("PAINT\n");
+			// NOTE(axel): Windows has the right to call it whenever it needs to.
+			// EndPaint say to Windows that you have finish to Paint it, if you don't 
+			// it will keep calling WM_PAINT. https://youtu.be/w7ay7QXmo_o?t=4867
 			PAINTSTRUCT Paint;
 			HDC DeviceContext = BeginPaint(Window, &Paint);
-
 			int X = Paint.rcPaint.left;
 			int Y = Paint.rcPaint.top;
-
 			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
 			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
 
-			Win32_window_dimensions Dimensions = GetWindowDimensions(Window);
-			Win32DisplayBufferInWindow(DeviceContext, Dimensions.Height, Dimensions.Width, GlobalBackBuffer);
+			Win32_window_dimensions Dimensions = Win32GetWindowDimensions(Window);
+			Win32DisplayBufferInWindow(DeviceContext, Dimensions.Width, Dimensions.Height, 
+										GlobalBackBuffer);
 			EndPaint(Window, &Paint);
 		}
 		default:
 		{
-			OutputDebugStringA("Default\n");
 			result = DefWindowProc(Window, Message, WParam, LParam);
 		} break;
 	}
@@ -152,6 +244,7 @@ LRESULT Win32MainWindowCallBack(
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 					 PSTR lpCmdLine, INT nCmdShow)
 {
+	LoadXInput();
 	WNDCLASS WindowClass = {};
 	WindowClass.hInstance = hInstance;
 	WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
@@ -172,32 +265,117 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 			0,
 			hInstance,
 			0);
+
+		Win32ResizeDIBSection(&GlobalBackBuffer, 800, 500);
+
 		if (Window)
 		{
 			HDC DeviceContext = GetDC(Window);
 			int XOffset = 0;
 			int YOffset = 0;
 
-			Running = true;
-			while (Running)
+			GlobalRunning = true;
+			while (GlobalRunning)
 			{
 				MSG Message;
 				while(PeekMessageA(&Message, 0, 0, 0, PM_REMOVE))
 				{
 					if (Message.message == WM_QUIT)
 					{
-						Running = false;
+						GlobalRunning = false;
 					}
 					TranslateMessage(&Message);
 					DispatchMessage(&Message);
 				}
 
+				for(uint16 ControllerIndex = 0;
+					 ControllerIndex < XUSER_MAX_COUNT;
+					 ++ControllerIndex)
+				{
+
+					XINPUT_STATE ControllerState;
+
+					if (XInputGetState(ControllerIndex, &ControllerState) == ERROR_SUCCESS)
+					{
+						// MessageText:
+						// The operation completed successfully (Controller connected).
+						XINPUT_GAMEPAD* Pad = &ControllerState.Gamepad;
+
+						bool DPadUp = Pad->wButtons & XINPUT_GAMEPAD_DPAD_UP;
+						bool DPadDown = Pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN;
+						bool DPadRight = Pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT;
+						bool DPadLeft = Pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT;
+						bool Start = Pad->wButtons & XINPUT_GAMEPAD_START;
+						bool Back = Pad->wButtons & XINPUT_GAMEPAD_BACK;
+						bool LeftThumb = Pad->wButtons & XINPUT_GAMEPAD_LEFT_THUMB;
+						bool RightThumb = Pad->wButtons & XINPUT_GAMEPAD_RIGHT_THUMB;
+						bool LeftShoulder = Pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER;
+						bool RightShoulder = Pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER;	
+						bool AButton = Pad->wButtons & XINPUT_GAMEPAD_A;
+						bool BButton = Pad->wButtons & XINPUT_GAMEPAD_B;
+						bool XButton = Pad->wButtons & XINPUT_GAMEPAD_X;
+						bool YButton = Pad->wButtons & XINPUT_GAMEPAD_Y;
+
+						if (AButton)
+						{
+							OutputDebugStringA("A \n");
+						} 
+						else if(BButton)
+						{
+						}
+						else if (XButton)
+						{
+						}
+						else if (DPadUp)
+						{
+						}
+						else if (DPadDown)
+						{
+						}
+						else if (DPadRight)
+						{
+						}
+						else if (DPadLeft)
+						{
+						}
+						else if (Start)
+						{
+						}
+						else if (Back)
+						{
+						}
+						else if (LeftThumb)
+						{
+						}
+						else if (RightThumb)
+						{
+						}
+						else if (LeftShoulder)
+						{
+						}
+						else if (RightShoulder)
+						{
+						}
+
+						uint16 StickX = Pad->sThumbLX;
+						uint16 StickY = Pad->sThumbLY;
+
+					}
+					else
+					{
+						// Controller is not connected
+					}
+					
+				}
 				RenderWeirdGradient(GlobalBackBuffer, XOffset, YOffset);
-				Win32_window_dimensions Dimensions = GetWindowDimensions(Window);
-				Win32DisplayBufferInWindow(DeviceContext, Dimensions.Width, Dimensions.Height, GlobalBackBuffer);
+				Win32_window_dimensions Dimensions = Win32GetWindowDimensions(Window);
+				Win32DisplayBufferInWindow(DeviceContext, 
+				Dimensions.Width, Dimensions.Height, 
+											GlobalBackBuffer);
 				ReleaseDC(Window, DeviceContext);
 
 				++XOffset;
+				YOffset += 2;
 			}
 		}
 	}
